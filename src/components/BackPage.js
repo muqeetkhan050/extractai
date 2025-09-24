6ee53505656ea3f29fc919579ff2b1bce3c375a7
muqeetkhan <muqeetkhan050@gmail.com>
2025-09-24 13:41:07 +1000

    completed layout

---
+++ src/components/BackPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Background from '../style/Background.css';
+const BackPage=()=>{
+    return (
+        <div className='Background'>
+            BackPage
+        </div>
+    )
+}
+
+export default BackPage;
