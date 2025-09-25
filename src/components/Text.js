18f3d57d8bb1197a2255d05529886b49fe5e07e3
muqeetkhan <muqeetkhan050@gmail.com>
2025-09-25 22:36:36 +1000

    woerked on main page, functions

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,20 +1,36 @@
 
+import React, { useState } from 'react';
 
-const Text=()=>{
-    return(
+const Text = () => {
+
+    const [image, setImage] = useState(null)
+    const [extractedText, setExtractedText] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [Progress, setProgress] = useState(0)
+    return (
         <div>
-        <div className="literata">
-            Images Speak. We Translate
-            <p/>
-            Print('Paste,wait,COpy')    
+            <div className="literata">
+                Images Speak. We Translate
+                <p />
+                Print('Paste,wait,COpy')
 
             </div>
-                <button >
-      Get Started
-    </button>
-        
-            
-</div>
+            <input type='file'></input>
+            <button >
+                Get Started
+            </button>
+
+            {image && (<img src={image} alt="Uploaded" />)}
+
+            {extractedText && (<textarea
+                value={extractedText}
+                readOnly
+                rows={10}
+                style={{ marginTop: "20px", width: "100%", padding: "10px", borderRadius: "8px" }}
+            />)}
+
+
+        </div>
     )
 }
 
