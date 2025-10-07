f34532ebdb66751bb7a02625188906e454a38646
muqeetkhan <muqeetkhan050@gmail.com>
2025-10-07 14:49:50 +1000

    worked with JWT

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import Main from './components/Main';
-// import Login from './components/Login';
-// import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-// import Register from './components/Register'; 
 
-// function App() {
-//   const isAuthenticated = localStorage.getItem("isAuthenticated");
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-
-  
-//     <Main />
-
-//     </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import './App.css';
 import Main from './components/Main';
 import Login from './components/Login';
 import Register from './components/Register';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   const isAuthenticated = localStorage.getItem("isAuthenticated");
@@ -34,11 +11,16 @@ function App() {
   return (
     <Router>
       {/* Example: Main as a navbar or sidebar */}
-      <Main />
-
+    
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+             <Route
+          path="/Main"
+          element={
+            isAuthenticated ? <Main /> : <Navigate to="/" replace />
+          }
+        />
       </Routes>
     </Router>
   );
