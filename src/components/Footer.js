e4918e75a0c526ce69ebf7d6aa38221de2c3ebff
muqeetkhan <muqeetkhan050@gmail.com>
2025-10-14 01:47:23 +1000

    functionalities completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
-function Footer(){
-    return (
-        <div>This is our footer</div>
-    )
 
-}
+import "../style/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+     
+
+        <div className="footer-credit">
+          <p>Developed by <span className="developer-name">Muqeet Khan</span> © 2025</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
